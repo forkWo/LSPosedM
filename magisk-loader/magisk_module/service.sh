f607b0c7dfe2240a9a8710198c33893d08a78e3b
forkWo <119728345+forkWo@users.noreply.github.com>
2024-03-20 21:46:49 +0800

    Add files via upload

--- magisk-loader/magisk_module/service.sh
+++ magisk-loader/magisk_module/service.sh
@@ -20,4 +20,37 @@
 MODDIR=${0%/*}
 cd "$MODDIR"
 # post-fs-data.sh may be blocked by other modules. retry to start this
+set_value() {
+  if [[ -f $2 ]]; then
+    chmod 644 $2 2>/dev/null
+    echo $1 > $2
+  fi
+}
+
+swapoff /dev/block/zram0 >/dev/null
+echo 1 > /sys/block/zram0/reset
+set_value lzo-rle /sys/block/zram0/comp_algorithm
+set_value 4G /sys/block/zram0/disksize
+mkswap /dev/block/zram0
+swapon /dev/block/zram0 >/dev/null
+set_value 1 /proc/sys/vm/page-cluster
+
 unshare --propagation slave -m sh -c "$MODDIR/daemon --from-service $@&"
+
+#开机30秒清理电池优化名单
+sleep 30
+#sh /storage/emulated/0/Android/data/moe.shizuku.privileged.api/start.sh
+#优化白名单，（添加应用包名后，将保留应用不被移出电池优化）
+#示例：+com.tencent.mm +后面是微信包名
+noDozes="
++com.tencent.mm
++com.xiaomi.xmsf
++com.mi.health
+"
+#执行移出电池优化名单
+noDozes=`pm list packages -e | sed "s/package:/-/g"`$noDozes
+dumpsys deviceidle whitelist $noDozes
+
+#MIUI安全组件
+pm disable com.miui.guardprovider/com.miui.guardprovider.manager.SecurityService
+pm disable com.miui.guardprovider/com.miui.guardprovider.manager.CacheBuildJobService
